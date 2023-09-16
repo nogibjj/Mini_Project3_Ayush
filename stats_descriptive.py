f0cd22118c6c691a2595d5708019e5c9f271cda9
ayushg245 <143531654+ayushg245@users.noreply.github.com>
2023-09-16 02:32:16 +0000

    Changed to Polars

--- stats_descriptive.py
+++ stats_descriptive.py
@@ -1,24 +1,21 @@
-import pandas as pd
-data = pd.read_csv("forbes_2022_billionaires.csv")
+
+import polars as pl
+
+data = pl.read_csv("forbes_2022_billionaires.csv")
 
 def stats_mean(df):
     return df['age'].mean()
   
-
 def stats_median(df):
-    return df["age"].median()
+    return df['age'].median()
   
-
 def stats_mode(df):
-    return df["age"].mode()
+    return df['age'].mode()[0]  
   
-
 def stats_std(df):
-    return df["age"].std()
+    return df['age'].std()
 
-print('mean =' + str(stats_mean(data)))
-print('median = ' +  str(stats_median(data)))
-print('mode ='+ str(stats_mode(data)))
-print('Standard_deviation ='+ str(stats_std(data)))
-
-
+print('mean =', stats_mean(data))
+print('median =', stats_median(data))
+print('mode =', stats_mode(data))
+print('Standard_deviation =', stats_std(data))
